efe3958345e642f80b7a816f3b005d38291978e5
Duvier <duvier.tavera01@unicatolica.edu.co>
2024-10-08 01:22:54 -0500

    se cambio el apuntador

--- src/components/UserHome.jsx
+++ src/components/UserHome.jsx
@@ -17,7 +17,7 @@ function UserHome({user}){
     async function handleSelect(event){
         const signo = event.target.value;
         if(signo!=="0"){
-            fetch(`https://horoscopo-front-dusky.vercel.app/v1/signos/${signo}`)
+            fetch(`https://horoscopo-back-sepia.vercel.app/v1/signos/${signo}`)
                 .then(response => response.json())
                 .then(responseData => setTextoSigno(responseData))
         } 
